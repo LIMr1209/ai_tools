6714e2ab29efda851b30fbe0251f3413ed09361f
LIMr1209 <aaa1058169464@126.com>
2021-03-14 14:54:50 +0800

    img

--- app/views/api/intelligent.py
+++ app/views/api/intelligent.py
@@ -75,6 +75,7 @@ class DrawUpload(MethodView):
         }
         return jsonify(**data)
 
+
 # 画笔或者上传生成
 @api.expose("/draw/generate")
 class DrawGenerate(MethodView):
@@ -116,7 +117,6 @@ class DrawGenerate(MethodView):
         return jsonify(**data)
 
 
-
 # 图片融合发散
 @api.expose("/fuse/divergence")
 class FuseDivergence(MethodView):
@@ -128,21 +128,22 @@ class FuseDivergence(MethodView):
         data = copy.deepcopy(dataInit)
         type = force_int(request.values.get("type", 1))
         index = force_int(request.values.get('index', 0))
-        img_1 = request.form.get('image_1_base64', '')
+        img_1 = request.values.get('image_1_base64', '')
         img_2 = request.values.get('image_2_base64', '')
         img_1 = img_1.replace("data:image/jpeg;base64,", "").replace("data:image/png;base64,", "")
         img_2 = img_2.replace("data:image/jpeg;base64,", "").replace("data:image/png;base64,", "")
-        # if not img_1 and not img_2:
-        #     data["meta"]["message"] = "图片参数错误"
-        #     data["meta"]["stat    us_code"] = 400
-        #     return data
-        # if type not in list(map(lambda x: x["id"], draw_generate_category())):
-        #     data["meta"]["message"] = "请选择正确的生成类型"
-        #     data["meta"]["status_code"] = 400
-        #     return data
-        img_1_base = pil_to_base64('static/image/1_1_result.jpg')
-        img_2_base = pil_to_base64('static/image/1_2_result.jpg')
-        img_3_base = pil_to_base64('static/image/1_3_result.jpg')
-        img_4_base = pil_to_base64('static/image/1_4_result.jpg')
-        data["data"] = [img_1_base,img_2_base,img_3_base,img_4_base]
+        file_name1 = request.values.get('file_name1', '')
+        file_name2 = request.values.get('file_name2', '')
+        data["data"] = []
+        if not file_name1 or not file_name2:
+            return jsonify(**data)
+        if not file_name1.endswith('_1_origin.png') or not file_name2.endswith('_2_origin.png'):
+            return jsonify(**data)
+        try:
+            group = file_name1.split('_')[0]
+        except:
+            return jsonify(**data)
+        for i in range(1, 5):
+            img = pil_to_base64('static/image/{}_{}_result.png'.format(group, i))
+            data["data"].append(img)
         return jsonify(**data)
