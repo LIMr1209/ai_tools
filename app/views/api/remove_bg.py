585fc8cb15a381cedaf4d305e60730975b21eff4
LIMr1209 <aaa1058169464@126.com>
2021-03-15 16:08:40 +0800

    removebg

--- app/views/api/remove_bg.py
+++ app/views/api/remove_bg.py
@@ -13,12 +13,14 @@ class ImageRemoveBg(MethodView):
     def post(self):
         data = copy.deepcopy(dataInit)
         f = request.files.get("file", None)
+        base64_data = request.values.get('image_base64','')
+        base64_data = base64_data.replace("data:image/jpeg;base64,", "").replace("data:image/png;base64,", "").replace("data:image/jpg;base64,", "")
         if not f:
             data["meta"]["message"] = "请上传图片文件"
             data["meta"]["status_code"] = 400
             return jsonify(**data)
         try:
-            success, base_64 = remove(image=f)
+            success, base_64 = remove(image=f,base64_data=base64_data)
             if success:
                 data['data'] = base_64
             else:
@@ -32,14 +34,12 @@ class ImageRemoveBg(MethodView):
     def get(self):
         data = copy.deepcopy(dataInit)
         url = request.args.get("url", "")
-        base64_data = request.args.get('image_base64','')
-        base64_data = base64_data.replace("data:image/jpeg;base64,", "").replace("data:image/png;base64,", "").replace("data:image/jpg;base64,", "")
         if not url:
             data["meta"]["message"] = "请传入图片链接"
             data["meta"]["status_code"] = 400
             return jsonify(**data)
         try:
-            success, data = remove(url=url,base64_data=base64_data)
+            success, data = remove(url=url)
             if success:
                 data['data'] = data
             else:
