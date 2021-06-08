0b6337c3e8d73bcf29d8321ec383b40df4c6e022
LIMr1209 <aaa1058169464@126.com>
2021-06-08 14:58:15 +0800

    bug

--- app/lib/dalle_pytorch/intelligent.py
+++ app/lib/dalle_pytorch/intelligent.py
@@ -1,11 +1,13 @@
 from pathlib import Path
 import torch
+from PIL import Image
 from torchvision.utils import make_grid
 from dalle_pytorch import OpenAIDiscreteVAE, DiscreteVAE, DALLE, VQGanVAE1024
 from dalle_pytorch.tokenizer import tokenizer
 from flask import current_app
 
 from app.helpers.common import img_to_base64
+from app.lib.cyclegan import tensor2im
 
 
 def generate_text(params):
@@ -60,6 +62,7 @@ def intelligent(params, type):
     dalle.load_state_dict(weights)
     text = tokenizer.tokenize(text_o, dalle.text_seq_len)
     output = dalle.generate_images(text, filter_thres=0.9)
-    image = make_grid(output.cpu()).numpy()
+    result = tensor2im(output.cpu())
+    image = Image.fromarray(result)
     image_base64 = img_to_base64(image)
     return image_base64
